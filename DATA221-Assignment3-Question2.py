754ce075c28db25b22869f634cd8b108b1cedec3
Abigail Spring <abigail.spring@ucalgary.ca>
2026-02-11 12:41:53 -0700

    Corrected an error

--- DATA221-Assignment3-Question2.py
+++ DATA221-Assignment3-Question2.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 crime_data_csv = pd.read_csv("crime1.csv")
 crime_column = crime_data_csv["ViolentCrimesPerPop"]
 plt.figure()
-plt.hist(crime_column, bin = 20)
+plt.hist(crime_column, bins = 20)
 plt.title("Histogram of Violent Crimes Per Population")
 plt.xlabel("Violent Crimes Per Population")
 plt.ylabel("Frequency")
