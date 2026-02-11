bfc011c44d605ebb510f628e08a79196e4be69b8
Abigail Spring <abigail.spring@ucalgary.ca>
2026-02-11 12:39:37 -0700

    Created the Code for the Boxplot of Violent Crimes Per Population. Also added some seperation for clarity

--- DATA221-Assignment3-Question2.py
+++ DATA221-Assignment3-Question2.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
 crime_data_csv = pd.read_csv("crime1.csv")
 crime_column = crime_data_csv["ViolentCrimesPerPop"]
 plt.figure()
@@ -8,3 +9,9 @@ plt.hist(crime_column, bin = 20)
 plt.title("Histogram of Violent Crimes Per Population")
 plt.xlabel("Violent Crimes Per Population")
 plt.ylabel("Frequency")
+
+plt.figure()
+plt.boxplot(crime_column)
+plt.title("Boxplot of Violent Crimes Per Population")
+plt.ylabel("Violent Crimes Per Population")
+plt.show()
