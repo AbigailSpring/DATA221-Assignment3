282121ee5c63f8c427b10b7b500e1fb2d9fc9d0f
Abigail Spring <abigail.spring@ucalgary.ca>
2026-02-11 10:30:39 -0700

    Calculated the mean, median, standard deviation, minimum value, and maximum value

--- DATA221-Assignment3-Question1.py
+++ DATA221-Assignment3-Question1.py
@@ -2,3 +2,8 @@ import pandas as pd
 import numpy as py
 crime_data_cvs = pd.read_cvs("crime.cvs")
 crime_column = crime_data_cvs["ViolentCrimesPerPop"]
+crime_data_mean = crime_column.mean()
+crime_data_median = crime_column.median()
+crime_data_standard_deviation = crime_column.std()
+crime_data_minimum_value = crime_column.min()
+crime_data_maximum_value = crime_column.max()
