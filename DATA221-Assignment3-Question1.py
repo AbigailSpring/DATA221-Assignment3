2ee99564b7664d7aa9264cf57fef5fe5aa979251
Abigail Spring <abigail.spring@ucalgary.ca>
2026-02-11 10:34:32 -0700

    Printing the resluts from the functions above

--- DATA221-Assignment3-Question1.py
+++ DATA221-Assignment3-Question1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as py
+
 crime_data_cvs = pd.read_cvs("crime.cvs")
 crime_column = crime_data_cvs["ViolentCrimesPerPop"]
 crime_data_mean = crime_column.mean()
@@ -7,3 +8,10 @@ crime_data_median = crime_column.median()
 crime_data_standard_deviation = crime_column.std()
 crime_data_minimum_value = crime_column.min()
 crime_data_maximum_value = crime_column.max()
+
+print("Results: ")
+print("Mean Value: ", crime_data_mean)
+print("Median Value: ", crime_data_median)
+print("Standard Deviation Value: ", crime_data_standard_deviation)
+print("Minimum Value: ", crime_data_minimum_value)
+print("Maximum Value: ", crime_data_maximum_value)
